2f26433cf247937373d8a2ca2da26d0d3cbe6180
stefanklut <stefanklut@users.noreply.github.com>
2018-10-19 16:46:57 +0200

    Now Front End has buttons !!!

--- search/search_engine/views.py
+++ search/search_engine/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def search(request):
 	if request.POST:
+		print(request.POST['Capital'])
 		return render_to_response('search.html', {'result': [request.POST['term']]})
 	else:
 		return render_to_response('search.html')
