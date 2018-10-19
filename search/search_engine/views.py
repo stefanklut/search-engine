e313cbf0d4536eeccec8fe6d933de05ec9a1680a
stefanklut <stefanklut@users.noreply.github.com>
2018-10-19 16:50:20 +0200

    removed a print statement

--- search/search_engine/views.py
+++ search/search_engine/views.py
@@ -6,7 +6,6 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def search(request):
 	if request.POST:
-		print(request.POST['Capital'])
 		return render_to_response('search.html', {'result': [request.POST['term']]})
 	else:
 		return render_to_response('search.html')
