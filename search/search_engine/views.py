21977381f933119a2f9852442f757e90b9756900
MeikeHK <39906797+MeikeHK@users.noreply.github.com>
2018-10-19 17:21:28 +0200

    now prints a list, so we can work on showing things that look like our results might

--- search/search_engine/views.py
+++ search/search_engine/views.py
@@ -6,6 +6,11 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def search(request):
 	if request.POST:
-		return render_to_response('search.html', {'result': [request.POST['term']]})
+		return render_to_response('search.html', {'result': get_correct_answers([request.POST['term']])})
 	else:
 		return render_to_response('search.html')
+
+
+def get_correct_answers(term):
+	thing = [['term', 245, 'how do I do this', 0.89], ['term', 245, 'how do I do this', 0.89]]
+	return thing
