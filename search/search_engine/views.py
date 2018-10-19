65319cd7434ced093b3600dee08d2d42e83e32cc
stefanklut <stefanklut@users.noreply.github.com>
2018-10-19 16:22:41 +0200

    Front end works on the most basic level

---
+++ search/search_engine/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def search(request):
+	if request.POST:
+		return render_to_response('search.html', {'result': [request.POST['term']]})
+	else:
+		return render_to_response('search.html')
